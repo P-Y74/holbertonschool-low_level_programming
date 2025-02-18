6e4b6c87e15e45eae32507d6f809026c21d73bf5
P-Y74 <197254317+P-Y74@users.noreply.github.com>
2025-02-18 14:59:36 +0100

    Create program that print single digit from 0 to 9

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all single digit numbers of base 10 starting from 0
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int digit;
+
+	digit = 0;
+	while (digit < 10)
+		printf("%d\n", digit++);
+
+	return (0);
+}
