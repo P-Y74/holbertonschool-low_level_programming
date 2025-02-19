0fd332648b40fc4c833d434c60b076d83d49bc6c
P-Y74 <197254317+P-Y74@users.noreply.github.com>
2025-02-19 11:08:36 +0100

    Create program that print _putchar

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -2,21 +2,20 @@
 #include "main.h"
 
 /**
- * toPrint - Print _putchar
- * @_putchar: The word to print
+ * main - Print _putchar
  *
- * Return: On success 1
+ * Return: 0
  */
 int main(void)
 {
-	_putchar(_);
-	_putchar(p);
-	_putchar(u);
-	_putchar(t);
-	_putchar(c);
-	_putchar(h);
-	_putchar(a);
-	_putchar(r);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
 
 	return (0);
