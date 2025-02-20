8015d59e459e353994543d28a45695831bd3d27d
P-Y74 <197254317+P-Y74@users.noreply.github.com>
2025-02-20 15:32:47 +0100

    Create fonction that print 9 times table

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * times_table - Prints the 9 table
+ *
+ * Return: void
+ */
+void times_table(void)
+{
+	int num1 = 0, num2 = 0, sum = 0;
+
+	while (num1 <= 9)
+	{
+		while (num2 <= 9)
+		{
+			sum = num1 * num2;
+			/*	_putchar((sum / 10) + '0');
+				_putchar((sum % 10) + '0');*/
+			if (sum < 10)
+			{
+				_putchar(' ');
+				_putchar((sum % 10) + '0');
+			}
+			else
+			{
+				_putchar((sum / 10) + '0');
+				_putchar((sum % 10) + '0');
+			}
+			if (num2 < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			num2++;
+		}
+		num2 = 0;
+		num1++;
+		_putchar('\n');
+	}
+}
