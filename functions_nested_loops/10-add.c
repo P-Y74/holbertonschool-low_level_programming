5283376d077ff8fd5cf6c87238ccc56c032822a8
P-Y74 <197254317+P-Y74@users.noreply.github.com>
2025-02-21 09:15:53 +0100

    Create fonction that adds two numbers

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * add - Adds to integers and returns the result
+ * @num1: first number to add
+ * @num2: second number to add
+ *
+ * Return: num1 + num2
+ */
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
