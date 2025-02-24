1d20903933b5430cfe521d723006146af42c2eea
P-Y74 <197254317+P-Y74@users.noreply.github.com>
2025-02-24 16:15:00 +0100

    Create function that prints a square

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_square - Prints a square
+ *
+ */
+void print_square(int size)
+{
+	int i = 0, j;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	while (i < size)
+	{
+		j = 0;
+		while (j < size)
+		{
+			_putchar('#');
+			j++;;
+		}
+		j = 0;
+		_putchar('\n');
+		i++;
+	}
+}
