08a3db4d75fc7c72326482b709b567ecca55b5a5
P-Y74 <197254317+P-Y74@users.noreply.github.com>
2025-02-25 09:54:05 +0100

    Create program that prints the numbers from 1 to 100 with multiple conditions

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Prints the numbers from 1 to 100
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int numbers;
+
+	for (numbers = 1 ; numbers <= 100 ; numbers++)
+	{
+		if (numbers % 3 == 0 || numbers % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (numbers % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (numbers % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%d", numbers);
+		}
+		if (numbers < 100)
+		{
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
