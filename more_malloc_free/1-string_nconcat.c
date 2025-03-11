9941139b7a7015bcaf0ee2edbc2283898ee7138b
P-Y74 <197254317+P-Y74@users.noreply.github.com>
2025-03-11 13:19:11 +0100

    Update documentation

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -20,41 +20,17 @@ int _strlen(char *s)
 }
 
 /**
- * *_strcat - Concatenates two strings
- * @dest: Destination buffer where src will be appended
- * @src: String to append to dest
- *
- * Return: Pointer to the resulting concatenated string (dest)
- */
-char *_strcat(char *dest, char *src)
-{
-	char *start = dest;
-
-	while (*dest)
-	{
-		dest++;
-	}
-	while (*src)
-	{
-		*dest = *src;
-		dest++;
-		src++;
-	}
-	*dest = '\0';
-	return (start);
-}
-
-/**
  * string_nconcat - Concatenate s1 and first n characters of s2
  * @s1: First string
  * @s2: Second string
  * @n: Number of bytes from s2 to concatenate
  *
- * Return: Pointer to the newly allocated concatenated string or NULL on failure
+ * Return: Pointer to the newly allocated concatenated string
+ * or NULL on failure
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int len1, len2, i;
+	unsigned int len1, len2, i, j;
 	char *concat;
 
 	if (s1 == NULL)
@@ -68,7 +44,6 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	len1 = _strlen(s1);
 	len2 = _strlen(s2);
-
 	if (n >= len2)
 	{
 		n = len2;
@@ -79,15 +54,16 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		return (NULL);
 	}
-	concat[0] = '\0';
 
-	_strcat(concat, s1);
-
-	for (i = 0; i < n; i++)
+	for (i = 0; i < len1; i++)
 	{
-		concat[_strlen(s1) + i] = s2[i];
+		concat[i] = s1[i];
 	}
-	concat[len1 + i] = '\0';
+	for (j = 0; j < n; j++)
+	{
+		concat[i + j] = s2[j];
+	}
+	concat[i + j] = '\0';
 
 	return (concat);
 }
