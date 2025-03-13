55daba5aac4a3b25cd1929f44b42dcb06e5ed71e
P-Y74 <197254317+P-Y74@users.noreply.github.com>
2025-03-13 12:21:51 +0100

    Update free_dog with condition if d != NULL

--- structures_typedef/5-free_dog.c
+++ structures_typedef/5-free_dog.c
@@ -3,14 +3,15 @@
 #include "dog.h"
 
 /**
- *
- *
- *
- *
+ * free_dog - Frees the memory allocated for a dog struct and its members
+ * @d: Pointer to the dog struct to free
  */
 void free_dog(dog_t *d)
 {
-	free(d->name);
-	free(d->owner);
-	free(d);
+	if (d != NULL)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
 }
