20ba4b7b2bb8baddaaf4a23cc322716e8882c9d6
P-Y74 <197254317+P-Y74@users.noreply.github.com>
2025-03-14 13:59:48 +0100

    Implement function to print a message to stderr

---
+++ hello_world/101-quote.c
@@ -0,0 +1,32 @@
+#include <unistd.h>
+
+/**
+ * _strlen - Calculates the length of a string.
+ * @s: Pointer to the string (constant).
+ *
+ * Return: The length of the string.
+ */
+int _strlen(const char *s)
+{
+	int length = 0;
+
+	while (s && *s++)
+	{
+		length++;
+	}
+	return (length);
+}
+
+/**
+ * main - Prints a message to the standard error (stderr).
+ *
+ * Return: 1, as specified in the requirements.
+ */
+int main(void)
+{
+	const char *message = "and that piece of art is useful\"- Dora Korpar, 2015 - 10 - 19 \n";
+
+	write(2, message, _strlen(message));
+
+	return (1);
+}
