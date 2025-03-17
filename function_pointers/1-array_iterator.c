d7ae96b55874c7fc02c3a02cf1fc49c72d65c15e
P-Y74 <197254317+P-Y74@users.noreply.github.com>
2025-03-17 12:32:08 +0100

    Add array_iterator function to apply a function on array elements

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * array_iterator - Applies a function to each element of an array
+ * @array: The array whose elements will be processed
+ * @size: The number of elements in the array
+ * @action: Pointer to a function that takes an int as a parameter and returns nothing
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
