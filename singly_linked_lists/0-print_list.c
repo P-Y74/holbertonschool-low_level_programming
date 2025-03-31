eccf191a05e4e2949bb37f47f30c944a66890d26
P-Y74 <197254317+P-Y74@users.noreply.github.com>
2025-03-31 11:58:36 +0200

    Update description

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -2,15 +2,16 @@
 #include "lists.h"
 
 /**
- * print_list - List to print contain in nodes
- * @h: Pointer that navigate into nodes
+ * print_list - Prints all elements of a linked list
+ * @h: Pointer to the first node of the list
  *
- * Return: count
+ * Return: The number of nodes in the list
  */
 size_t print_list(const list_t *h)
 {
 	int count = 0;
 
+	/** Traverse the list and print each node's content */
 	while (h != NULL)
 	{
 		if (h->str == NULL)
