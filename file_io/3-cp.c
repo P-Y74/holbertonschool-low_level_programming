d9841acda4a9671c34114170d70567a821d50e16
P-Y74 <197254317+P-Y74@users.noreply.github.com>
2025-04-04 18:38:03 +0200

    Update by trying to fix the read return value error

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -33,7 +33,7 @@ void copy_content_file_to_another_file(const char *file_from, char *file_to)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
 		exit(98); }
-	fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	fd_to = open(file_to, O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (fd_to == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
