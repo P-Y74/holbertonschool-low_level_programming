936d9385a5eae4ed3c64541739866fd2760d0ce5
P-Y74 <197254317+P-Y74@users.noreply.github.com>
2025-04-04 11:15:56 +0200

    feat: implement file copy functionality
    - Adds a program that copies the content of one file to another.
    - Handles errors such as incorrect arguments, file read/write issues, and file descriptor closing failures.
    - Ensures proper file permissions (rw-rw-r--).

---
+++ file_io/3-cp.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdarg.h>
+
+/**
+ * copy_content_file_to_another_file - Copies content from one file to another.
+ * @file_from: The source file name.
+ * @file_to: The destination file name.
+ */
+void copy_content_file_to_another_file(const char *file_from, char *file_to)
+{
+	ssize_t openFile, createFile, writeFile, readBytes;
+	char buffer[1024];
+
+	openFile = open(file_from, O_RDONLY);
+	createFile = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (file_from == NULL || openFile == -1)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(98); }
+	if (createFile == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s", file_to);
+		close(createFile);
+		exit(99); }
+	while ((readBytes = read(openFile, buffer, 1024)) > 0)
+	{
+		writeFile = write(createFile, buffer, readBytes);
+		if (writeFile == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
+			close(openFile);
+			close(createFile);
+			exit(99);
+		}
+	}
+	if (readBytes == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+		close(openFile);
+		close(createFile);
+		exit(98); }
+	if (close(openFile) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %ld\n", openFile);
+		exit(100); }
+	if (close(createFile) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %ld\n", createFile);
+		exit(100); }
+}
+
+/**
+ * main - Copies the content of a file to another file.
+ * @argc: The number of arguments passed to the program.
+ * @argv: An array of strings representing the command-line arguments.
+ *
+ * Return: Always 0 on success, or one of the following error codes:
+ *         - 97: Incorrect number of arguments.
+ *         - 98: Error reading from the source file.
+ *         - 99: Error writing to the destination file.
+ *         - 100: Error closing a file descriptor.
+ */
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	copy_content_file_to_another_file(argv[1], argv[2]);
+
+	return (0);
+}
